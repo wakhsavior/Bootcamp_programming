9d5e2696c7045d44e67eb7b7a405180f7cf42603
wakhsavior <wakhsavior@gmail.com>
2022-08-24 00:17:55 +0300

    Add lectures 02,03

---
+++ Lecture_02/Program.cs
@@ -0,0 +1,20 @@
+﻿// НОД Алгоритм Евклида
+// НОД (14, 21) = 7
+
+int n = 45;
+int m = 78;
+
+int prod = m * n;
+
+while (n != m)
+{
+    if (n > m)
+        n = n - m;
+    else
+        m = m - n;
+    //    Console.WriteLine($"{n}\t {m}");
+}
+
+Console.WriteLine($"НОД = {n}");
+Console.WriteLine($"НОК = {prod/n}");
+
---
+++ Lecture_03/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = (i + 1) * (j + 1);
+        matrix[j, i] = (i + 1) * (j + 1);
+    }
+
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{matrix[i, j]}\t");
+    }
+    Console.WriteLine();
+}
+
+
