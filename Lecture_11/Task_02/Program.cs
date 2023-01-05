bdb0559765e90085beddd744065206057c14ca21
wakhsavior <wakhsavior@gmail.com>
2023-01-05 14:08:32 +0100

    Lecture_11 start

---
+++ Lecture_11/Task_02/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.Write("Enter the N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i<=N;i++)
+{
+    collatz(i);
+    Console.WriteLine();
+}
+int collatz(int n)
+{
+    if (n == 1)
+    {
+        Console.Write("0-");
+        return 0;
+    }
+    else if ((n % 2) == 0)
+    {
+         Console.Write($"{1+collatz(n / 2)}-");
+        return 1 + collatz(n / 2);
+    }
+    else
+    {
+        Console.Write($"{3*n+1}-");
+        return 1 + collatz(3 * n + 1);
+    }
+}
